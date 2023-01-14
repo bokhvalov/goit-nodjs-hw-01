bfc35ef4ce33a10be1e20b79c850c62b10f4c741
Dev_7 <Dev_6>
2023-01-14 16:29:55 +0100

    end of dev

---
+++ README.md
@@ -0,0 +1,11 @@
+node index.js --action list
+https://monosnap.com/file/KDfIyV7ArJDUswnPItqWBUUwUsTmf0
+
+node index.js --action get --id 5
+https://monosnap.com/file/aEkalXXCjkXAFkTgyti9BsIxsmRzMZ
+
+node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22
+https://monosnap.com/file/taSJlw3yPq5M19Olz8olHNXHx4SMb1
+
+node index.js --action remove --id=3
+https://monosnap.com/file/omq34BF3gHogjhpBMlgfbBGfd28V6Q
--- contacts.js
+++ contacts.js
@@ -8,39 +8,51 @@ async function listContacts() {
   return JSON.parse(listOfContacts);
 }
 
+
+
 async function getContactById(contactId) {
-  const listOfContacts = await fs.readFile(contactsPath, "UTF-8");
-  const parsedContacts = JSON.parse(listOfContacts);
+  const parsedContacts = await listContacts();
+
   const contact = parsedContacts.find((contact) => contact.id === contactId);
   return contact;
 }
 
+
+
 async function removeContact(contactId) {
-  const listOfContacts = await fs.readFile(contactsPath, "UTF-8");
-  const parsedContacts = JSON.parse(listOfContacts);
+  const parsedContacts = await listContacts();
+
   const filteredContacts = parsedContacts.filter(
-    (contact) => contact.id !== contactId
+    ({id}) => id !== contactId
   );
   fs.writeFile(contactsPath, JSON.stringify(filteredContacts));
+
+  console.log(`Contact with id #${contactId} successfully deleted, the db updated`);
+  return await listContacts();
 }
 
+
+
 async function addContact(name, email, phone) {
-  const listOfContacts = await fs.readFile(contactsPath, "UTF-8");
-  const parsedContacts = JSON.parse(listOfContacts);
+  const parsedContacts = await listContacts();
 
   //looking for the biggest id number (could be replaced by some id generator)
   let biggestId = 0;
   parsedContacts.map(({ id }) => (+id > biggestId ? (biggestId = +id) : null));
+  const newContactID = biggestId + 1;
 
   parsedContacts.push({
-    id: (biggestId + 1).toString(), //converting to String to save DB var's type from example
+    id: newContactID.toString(), //converting to String to save DB var's types from example
     name: name.toString(),
     email: email.toString(),
     phone: phone.toString(),
   });
-
   fs.writeFile(contactsPath, JSON.stringify(parsedContacts));
+
+  console.log(`${name} successfully added to DB with id ${newContactID}`)
+  return await listContacts();
 }
+
 
 module.exports = {
   listContacts,
